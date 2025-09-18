74475db92441805fce577bb1a0c345d65c1f664f
Cr1sHB <142345588+Cr1sHB@users.noreply.github.com>
2025-09-17 18:15:09 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
     'fetch',
     function(event){
         if (/\.jpg$/.test(event.request.url)){
-            event.respondWith(fetch('silksong.jpg'));
+            event.respondWith(fetch('unicorn.jpg'));
         }
     }
 );

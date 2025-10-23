06557824892704033db66c7faa96212652255a34
Cr1sHB <142345588+Cr1sHB@users.noreply.github.com>
2025-10-22 18:40:45 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,9 @@
-self.addEventListener(
-   'fetch',
-   function(event){
-      if (/\.jpg$/.test(event.request.url)){
-         event.respondWith(
-            new Response('<p> Esta respuesta es personalizada, es envíada por el Service Worker </p>',
-                         {
-                            headers: {'Content-Type':'text/html'}
-                         });
-         );
-     }
-   });
+self.addEventListener('fetch', function(event) {
+   if (/\.jpg$/.test(event.request.url)) {
+      event.respondWith(
+         new Response('<p>This is a response that comes from your service worker!</p>', {
+            headers: { 'Content-Type': 'text/html' }
+         });
+      );
+   }
+});
